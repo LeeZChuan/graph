73d2966a99ebaa8c083d3727ae265328796e7c82
lzc <cdutlzc@gmail.com>
2022-05-06 14:09:05 +0800

    update math

--- src/chart/modules/lib/Chart.ts
+++ src/chart/modules/lib/Chart.ts
@@ -1,8 +1,9 @@
-import { init, ZRenderType } from "zrender";
+import { Group, init, ZRenderType, Rect as ZRRect } from "zrender";
 import Eventful from "zrender/lib/core/Eventful";
 import { Layout, LayoutPosition } from "../layout/Layout";
 import { Element } from "./Element";
 import Event from './Event';
+import { getRandomColor } from '../util/math';
 export interface ChartOption {
   dpr?: number;
   render?: "svg" | "canvas";
@@ -45,52 +46,81 @@ export class Chart extends Eventful<{ ready: () => void }> {
   option: ChartOption;
   // 图表组件集合
   elements = new Map<string, Element>();
+  // 事件管理
   event: Event;
+
+  // 分辨率
   get dpr() {
     return this.option.dpr || 2;
   }
+
+  // 渲染方式
   get render() {
     return this.option.render || "canvas";
   }
+
+  // 布局对象
   get layout() {
     return this.option.layout || new Layout([10, "auto", 10], [10, "auto", 10]);
   }
-  add(part: Element, position: LayoutPosition) {
-    if (!this.elements.has(part.name)) {
-      this.elements.set(part.name, part);
-      this.zr.add(part.shape);
-      part.onAdd(this, position);
+
+  //添加一个图表组件
+  add<T extends Element>(part: Element, position: LayoutPosition) {
+    // if (!this.elements.has(part.name)) {
+    //   this.elements.set(part.name, part);
+    //   this.zr.add(part.shape);
+    //   part.onAdd(this, position);
+    // }
+
+    const old = this.elements.get(part.name);
+    if (old) {
+      this.remove(old);
     }
+    this.elements.set(part.name, part);
+    //旧版add
+    part.shape && this.zr.add(part.shape);
+    //重构后的add
+    part._view && this.zr.add(part._view.group);
+    part.onAdd(this, position);
+    return part;
   }
+
+  // 移除一个图表组件
   remove(part: Element) {
     this.elements.delete(part.name);
     this.zr.remove(part.shape);
     part.onRemove();
   }
+
+  // 图表组件安装完成，调用ready，根据获取的dom高度宽度信息，统一更新各个组件所处的布局信息，然后将组件内容进行更新
   ready() {
-    this.grid = this.layout.getGrid(
-      this.zr.dom.offsetWidth,
-      this.zr.dom.offsetHeight
-    );
+    this.grid = this.layout.getGrid(this.zr.dom.offsetWidth, this.zr.dom.offsetHeight);
     this.elements.forEach((part) => {
+      // 调用所有组件的更新方法
       part.update();
     });
   }
+
+  // 重新调整画布尺寸
+  resize() {
+    this.zr.resize();
+    this.ready();
+  }
+
+  // 获取包围盒的画布坐标
   getRect(position: LayoutPosition) {
     let c1: number, c2: number, r1: number, r2: number;
     if (typeof position[0] == "object") {
-      c1 = position[0][0];
-      c2 = position[0][1];
+      [[c1, c2]] = position;
     } else {
-      c1 = position[0];
-      c2 = position[0];
+      [c1] = position;
+      c2 = c1;
     }
     if (typeof position[1] == "object") {
-      r1 = position[1][0];
-      r2 = position[1][1];
+      [, [r1, r2]] = position;
     } else {
-      r1 = position[1];
-      r2 = position[1];
+      [, r1] = position;
+      r2 = r1;
     }
     const x = this.grid.xs[c1];
     const y = this.grid.ys[r1];
@@ -100,5 +130,31 @@ export class Chart extends Eventful<{ ready: () => void }> {
       width: this.grid.xs[c2 + 1] - x,
       height: this.grid.ys[r2 + 1] - y,
     };
+  }
+
+  // 查看布局，用于调试
+  showLayout(delay = 10000) {
+    const group = new Group();
+    for (let i = 0; i < this.grid.xs.length - 1; i++) {
+      const x0 = this.grid.xs[i];
+      const x1 = this.grid.xs[i + 1];
+      for (let j = 0; j < this.grid.ys.length - 1; j++) {
+        const y0 = this.grid.ys[j];
+        const y1 = this.grid.ys[j + 1];
+        group.add(new ZRRect({
+          silent: true,
+          shape: {
+            x: x0,
+            y: y0,
+            width: x1 - x0,
+            height: y1 - y0
+          },
+          style: {
+            fill: getRandomColor(),
+            opacity: 0.5
+          }
+        }))
+      }
+    }
   }
 }
---
+++ src/chart/modules/util/math.ts
@@ -0,0 +1,113 @@
+import { stringify } from "zrender/lib/tool/color";
+
+export function getRange(data: number[]): [number, number] {
+  let filterData = data.filter(item => !isNaN(parseFloat(String(item))));
+  filterData = filterData.map(item => parseFloat(String(item)));
+  if (filterData.length > 0) {
+    let min = filterData[0];
+    let max = filterData[0];
+    filterData.forEach((e) => {
+      if (e < min) {
+        min = e;
+      } else if (e > max) {
+        max = e;
+      }
+    });
+    return [min, max];
+  } else {
+    throw new Error("空数组");
+  }
+}
+
+/**
+ * 获取扩张返回之后的Y轴的最大最小值
+ * @param min 最小值
+ * @param max 最大值
+ * @returns
+ */
+export function getExpandRange(min: number, max: number): number[] {
+  if (min >= max) {
+    return [min, max];
+  }
+  // 分割线数量
+  const splitCount = 6;
+  const finalmin = min > 0 ? 0 : min;
+  const finalmax = max < 0 ? 0 : max;
+  const resultRange = [];
+  if (finalmax - finalmin > 1) {
+    const step = Math.ceil(((finalmax - finalmin) / (splitCount - 1)) * (1 + 0.05));
+    resultRange[0] = Math.floor(finalmin);
+    resultRange[1] = Math.floor(finalmin) + step * (splitCount - 1);
+  } else {
+    const step = ((finalmax - finalmin) / (splitCount - 1)) * (1 + 0.05);
+    resultRange[0] = Number(finalmin);
+    resultRange[1] = Number((finalmin) + step * (splitCount - 1));
+  }
+  return resultRange;
+}
+
+
+/**
+ * 对一个区间做分割
+ * @param min 区间最小值
+ * @param max 区间最大值
+ * @param splitCount 分割数量,
+ * @param expandRate 向上扩展比例,
+ * @returns 划分区间
+ */
+export function getExpandSplit(min: number, max: number, splitCount: number, expandRate: number): number[] {
+  if (min === max) {
+    return Array(splitCount).fill(min);
+  }
+
+  if (min >= max) {
+    throw new Error("区间不合理");
+  }
+
+  const split: number[] = [];
+  if (max - min > 1) {
+    const step = Math.ceil(((max - min) / (splitCount - 1)) * (1 + expandRate));
+    for (let i = 1; i <= splitCount; i++) {
+      split.push(Number(Math.floor(min) + step * (i - 1)));
+    }
+  } else {
+    const step = ((max - min) / (splitCount - 1)) * (1 + expandRate);
+    for (let i = 1; i <= splitCount; i++) {
+      split.push(Number(min + step * (i - 1)));
+    }
+
+    return split;
+  }
+
+  return split;
+}
+
+
+/**
+ * 对一个区间做分割,分割间距保持一位有效数字,结果区间覆盖传入区间
+ * @param min 最小值
+ * @param max 最大值
+ * @param count 分割数,
+ * @returns
+ */
+export function getSplitValue(min: number, max: number, count: number) {
+  if (min >= max) {
+    throw new Error("区间不合理");
+  }
+
+  const step = tick((max - min) / (count - 1));
+  const res: number[] = [];
+
+  let value = Math.floor(min / step) * step;
+  while (value < max) {
+    res.push(value);
+    value += step;
+  }
+  res.push(value);
+  return res;
+}
+
+export function tick(a: number) {
+  const step = Math.pow(10, Math.floor(Math.log10(a)));
+  return Math.ceil(a / step) * step;
+}
